9079448719ba852eae9550cfa6a73f4c2631aad6
bernar_berdicool <Dabeerman32215@gmail.com>
2025-05-12 02:39:55 +0500

    3-vite-js: Add slider in works section.

--- 3-vite-js/src/js/script.js
+++ 3-vite-js/src/js/script.js
@@ -1,4 +1,37 @@
 import "purecss/build/grids-min.css";
 import "purecss/build/grids-responsive-min.css";
 
+import Swiper from 'swiper';
+import {Navigation, Pagination} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import "/src/sass/style.scss";
+
+try {
+    new Swiper('.works__slider', {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            // when window width is >= 1200px
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+            },
+            // when window width is >= 1920px
+            1920: {
+                spaceBetween: 35,
+            },
+        },
+        modules: [Navigation, Pagination],
+    });
+} catch (e) {
+}
